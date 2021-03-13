eefa002e9c30b0b85f2d1006806f2682cdfd22fd
Алексей Белов <alexei_bam@mail.ru>
2021-03-14 00:00:29 +0600

    Добавил класс Adress

---
+++ KaspiLab05/Objects/Adress.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KaspiLab05.Objects
+{
+    enum City
+    {
+        Almaty,
+        Astana,
+        Turkestan
+    }
+    enum Street
+    {
+        Sultan,
+        Amangeldy,
+        Suyunbaya
+    }
+    class Adress
+    {
+        public City city = new City();
+        public Street street = new Street();
+        public int num;
+        public Adress(City C, Street S, int N)
+        {
+            city = C;
+            street = S;
+            num = N;
+
+        }
+    }
+}
