9ee185b2b187ab36001026b75d87033681337940
Алексей Белов <alexei_bam@mail.ru>
2021-03-15 00:30:06 +0600

    реализовал все функции(Закончено)

--- KaspiLab05/Program.cs
+++ KaspiLab05/Program.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 
 namespace KaspiLab05
 {
+
     class Program
     {
         static Random rand = new Random();
@@ -14,22 +15,26 @@ namespace KaspiLab05
         static Random S = new Random();
         static Random N = new Random();
         static Random P = new Random();
-
+        public static List<Product> list_prod = new List<Product>();
+        static int cursor_L;
+        static int cursor_T;
         static void Main(string[] args)
         {
+
+
             string[] surname = new string[5] {"Букин", "Софиулин","Кузнецов","Ледошляп","Захаранс"};
             string[] name = new string[5] { "Генадий", "Анатолий", "Игорь", "Артем", "Максим" };
             string[] patronymic = new string[5] { "Артемович", "Михайлович", "Алесандрович", "Русланович", "Павлович" };
 
 
 
-            Storage Damu_Logistic = new ClosedStorage() { sqгare = 230, storage_ID = 1};
-            Storage ZIP_Logistic = new OpenStorage() { sqгare = 530, storage_ID=2};
-            Storage Admart = new ClosedStorage() {  sqгare = 320, storage_ID=3};
+            Storage Damu_Logistic = new ClosedStorage() { sqгare = 230, name = "Damu_Logistic" };
+            Storage ZIP_Logistic = new OpenStorage() { sqгare = 530, name = "ZIP_Logistic" };
+            Storage Admart = new ClosedStorage() {  sqгare = 320, name = "Admart" };
 
 
 
-            List<Storage> storages = new List<Storage>() { Damu_Logistic,ZIP_Logistic,Admart };
+            List <Storage> storages = new List<Storage>() { Damu_Logistic,ZIP_Logistic,Admart };
 
 
 
@@ -70,14 +75,19 @@ namespace KaspiLab05
 
             Product burger = new Solid(42069, "Burger king", 990, "Women belongs in the kitchen",ref ZIP_Logistic);// Sorry, i just like memes
 
-
-
+            //KeyValuePair<Product,int> pro = Damu_Logistic.products.First();
+            //Console.WriteLine(list_prod[1].cost);
+           // Console.WriteLine(pro.Key.cost);
+            // cola.cost = 200;
+            //Console.WriteLine(pro.Key.cost);
+            //Console.WriteLine(list_prod[1].cost);
             while (true)
             {
                 Console.WriteLine("Выберите склад");
                 Console.WriteLine("1-Damu Logistic\n2-ZIP Logistic\n3 Admart");
                 int select = Convert.ToInt32(Console.ReadLine())-1;
                 Console.Clear();
+                Console.WriteLine(storages[select].name);
                 Console.WriteLine("Адрес: {0} {1} {2}",storages[select].adress.city, storages[select].adress.street, storages[select].adress.num);
                 Console.WriteLine("Площадь: {0}", storages[select].sqгare);
                 Console.WriteLine("Тип склада: {0}", (storages[select] is OpenStorage? ("Открытый"):("Закрытый")));
@@ -111,18 +121,26 @@ namespace KaspiLab05
                 Console.WriteLine("\n\n Общая сумма товаров:   " + storages[select].Cost_ptoduct()+"тнг");
 
 
-
-                Console.SetCursorPosition(0, 25);
-                Console.Write("1- Поиск товара\t2-Добавить товар\t3-Переместить товар");
+                cursor_L = Console.CursorLeft;
+                cursor_T = Console.CursorTop;
+                Console.SetCursorPosition(0, cursor_T+5);
+                Console.Write("1- Поиск товара\t 2-Добавить товар\t3-Переместить товар\t4-Сумма всех складов \t0-назад\n");
                 int check = Convert.ToInt32(Console.ReadLine());
                 int SKU;
-                Console.Clear();
+                int count;
                 switch (check)
                 {
-                   
+                    case 0:
+                        Console.Clear();
+                        break;
                     case 1:
                         {
-                            Console.WriteLine("Введите SKU");
+                            Console.Clear();
+                            foreach(Product prod in list_prod)
+                            {
+                                Console.WriteLine(prod.SKU + " " + prod.name);
+                            }
+                            Console.WriteLine("\nВведите SKU товара, который хотите найти");
                             SKU = Convert.ToInt32(Console.ReadLine());
                             if (storages[select].Search_SKU(SKU)==null)
                             {
@@ -135,7 +153,7 @@ namespace KaspiLab05
                                         Tuple<Product,int> found_prod = S.Search_SKU(SKU);
                                         if (found_prod!=null)
                                         {
-                                            Console.WriteLine(S.adress+ ":" + found_prod.Item1.name +" " + found_prod.Item2);//Добавлю имя склада
+                                            Console.Write(S.name+ ": " + found_prod.Item1.name +" " + found_prod.Item2);//Добавлю имя склада
                                             if (found_prod.Item1 is Liquid liquid)
                                             {
                                                 Console.Write(liquid.unit);
@@ -148,10 +166,11 @@ namespace KaspiLab05
                                             {
                                                 Console.Write(granular.unit);
                                             }
+                                            Console.WriteLine();
                                         }
                                         else
                                         {
-                                            Console.WriteLine(S.adress + ": Данного товара нет");
+                                            Console.WriteLine(S.name + ": Данного товара нет");
                                         }
                                     }
                                 }
@@ -164,9 +183,71 @@ namespace KaspiLab05
                         }
                     case 2:
                         {
+                            Console.Clear();
+                            foreach (Product prod in list_prod)
+                            {
+                                Console.WriteLine(prod.SKU + " " + prod.name);
+                            }
                             Console.WriteLine("Введите SKU товара, который хотите добавить");
                             SKU =Convert.ToInt32( Console.ReadLine());
-                            
+                            foreach(Product P in list_prod)
+                            {
+                                if (P.SKU == SKU)
+                                {
+
+
+                                    
+                                    Console.WriteLine(P.name + " Введите количество");
+                                    count = Convert.ToInt32(Console.ReadLine());
+                                    Console.WriteLine(storages[select].Add_product(P, count)==true?("Товар добавлен"):("ошибка"));
+                                   
+                                    
+                                    // KeyValuePair<Product, int> pro = Damu_Logistic.products.Last();
+
+                                    // Console.WriteLine(pro.Key.cost);
+                                    // burger.cost = 10;
+                                    // Console.WriteLine(pro.Key.cost);
+                                    // Console.ReadLine();
+                                    // Закоменчена проверка на то, что класс склада хранит только ссылку на объект
+                                    break;
+                                }
+                                
+                            }
+                            Console.WriteLine("Нажмите любую кнопку для продолжения");
+                            Console.ReadKey();
+                            Console.Clear();
+                            break;
+                        }
+                    case 3:
+                        {
+                            Console.WriteLine("Введите SKU товара:");
+                            SKU = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("Введите количество товара:");
+                            count = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("Выберите место доставки:");
+                            for (int i = 0; i<storages.Count();i++)
+                            {
+                                Console.WriteLine(i+1 + " " + storages[i].name);
+                            }
+                            check = Convert.ToInt32(Console.ReadLine())-1;
+                            storages[select].Transfer(storages[check],storages[select].Search_SKU(SKU).Item1,count);
+                           // buckwheat.cost = 20; просто поверка ссылки
+                            break;
+                        }
+                    case 4:
+                        {
+                            decimal sum=0;
+                            Console.Write("\nСумма всех складов равна: ");
+                            foreach(Storage stor in storages)
+                            {
+                                sum += stor.Cost_ptoduct();
+                            }
+                            Console.Write(sum+"\n");
+
+                            sum = 0;
+                            Console.WriteLine("Нажмите любую кнопку для продолжения");
+                            Console.ReadKey();
+                            Console.Clear();
                             break;
                         }
                 }
