4e48c592f3aa60646f45fe60954e8e5d267371c7
Алексей Белов <alexei_bam@mail.ru>
2021-04-10 14:19:39 +0600

    переарботал асинхронный метод и добавил синхронный для проверки

--- MD5_Test/Program.cs
+++ MD5_Test/Program.cs
@@ -15,28 +15,22 @@ namespace MD5_Test
             string newstring = "Give me two number nine, a number nine large, a number six with extra dip, a number seven , two number fortifive , one with chees, and a large soda.... diet";
             Stopwatch stopwatch = new Stopwatch();
             stopwatch.Start();
-            Task.WaitAll(GetHash(6000, newstring));
+            Task task = GetHash(6000, newstring);
+            GetHashSync(6000, newstring);
+            Task.WaitAll(task);
             stopwatch.Stop();
             TimeSpan ts = stopwatch.Elapsed;
             string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
             ts.Hours, ts.Minutes, ts.Seconds,
             ts.Milliseconds);
-            Console.WriteLine("RunTime " + elapsedTime);
-            Console.ReadKey();
-            stopwatch.Reset();
-            stopwatch.Start();
-            GetHashSync(6000, newstring);
-            stopwatch.Stop();
-            ts = stopwatch.Elapsed;
-            elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
-            ts.Hours, ts.Minutes, ts.Seconds,
-            ts.Milliseconds);
-            Console.WriteLine("RunTime " + elapsedTime);
+            Console.WriteLine("ASYNC RunTime " + elapsedTime);
+            
             Console.ReadKey();
         }
         
         public static async Task GetHash(int count, string input)
         {
+            
             byte[] hash = null;
             int i=0;
             Task[] tasks = new Task[count];
@@ -49,15 +43,18 @@ namespace MD5_Test
                     {
                         var md5 = MD5.Create();
                         hash = md5.ComputeHash(Encoding.UTF8.GetBytes(input));
-                        Console.WriteLine(Task.CurrentId-2 + "\t" + Convert.ToBase64String(hash));
+                        Console.WriteLine("ASYNC " + (Task.CurrentId-2) + "\t" + Convert.ToBase64String(hash));
                     }));
                     i++;
                 }
+                Task.WaitAll(tasks);
             });
-            //Task.WaitAll(tasks);
+            
         }
         public static void GetHashSync(int count, string input)
         {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
             byte[] hash = null;
             int i = 0;
 
@@ -66,10 +63,15 @@ namespace MD5_Test
                 //Console.WriteLine("i= "+ i);
                 var md5 = MD5.Create();
                 hash = md5.ComputeHash(Encoding.UTF8.GetBytes(input));
-                Console.WriteLine(Task.CurrentId-2 + "\t" + Convert.ToBase64String(hash));
+                Console.WriteLine(i+"\t" + Convert.ToBase64String(hash));
                 i++;
             }
-
+            stopwatch.Stop();
+            TimeSpan ts = stopwatch.Elapsed;
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+            ts.Hours, ts.Minutes, ts.Seconds,
+            ts.Milliseconds);
+            Console.WriteLine("SYNC RunTime " + elapsedTime);
         }
     }
 }
