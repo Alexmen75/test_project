4f85788d602e4d27da12c1e18c5bda84de759509
Алексей Белов <alexei_bam@mail.ru>
2021-04-18 01:59:43 +0600

    Добавил работу с таблицей AdventureWorks2019

---
+++ AdventureWorks2019 DB/Program.cs
@@ -0,0 +1,32 @@
+﻿using AdventureWorks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventureWorks2019_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var DB = new Model1())
+            {
+                try
+                {
+                    Console.WriteLine("Code    \tName\n");
+                    foreach (var Prod in DB.Product)
+                    {
+                        Console.WriteLine(Prod.ProductNumber + "    \t" + Prod.Name);
+                    }
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                
+            }
+        }
+    }
+}
